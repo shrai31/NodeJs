a618e378d059bb4594c2b90ccfd1f9a4fec8ef7e
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-09-11 01:39:00 +0530

    initially setup is done mongo with mvc flow 117

--- TaskManager/app.js
+++ TaskManager/app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
-const tasks = require('./routes/tasks')
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connectDb');
+require('dotenv').config();
 
-const port = 3000;
+const port = 4200;
 
+app.use(express.static('./public'));
 app.use(express.json());
 
-app.use('/api/v1/task', tasks)
+app.use('/api/v1/tasks', tasks);
 
-app.listen(port,console.log(`Server is listing on ${port}...`))
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listing on ${port}...`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
