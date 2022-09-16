b84391a0708a7e51d746e21fec5c1d561f3860d1
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-09-17 00:23:15 +0530

    not found page

--- TaskManager/app.js
+++ TaskManager/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connectDb');
+const notFound = require('./middleware/not-found')
 require('dotenv').config();
 
 const port = 4200;
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use('/api/v1/tasks', tasks);
 
+app.use(notFound)
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
