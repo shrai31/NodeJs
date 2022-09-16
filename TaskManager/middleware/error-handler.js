80a09b6135875e6d150fcbb8cd0064f370e8bd6a
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-09-17 00:55:39 +0530

    using the error of constructor

--- TaskManager/middleware/error-handler.js
+++ TaskManager/middleware/error-handler.js
@@ -1,5 +1,7 @@
 const errorHandlerMiddleware = (err, req, res, next) => {
-  return res.status(500).json({ msg: "Something went wrong" });
+  console.log({ err });
+  //   return res.status(500).json({ msg: "Something went wrong" });
+  return res.status(500).json({ msg: err.message });
 };
 
 module.exports = errorHandlerMiddleware;
