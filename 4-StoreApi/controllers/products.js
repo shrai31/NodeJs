42104752c6b4ba1407c832743ce5727da27b5502
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-09-17 18:42:46 +0530

    in all products

--- 4-StoreApi/controllers/products.js
+++ 4-StoreApi/controllers/products.js
@@ -1,11 +1,12 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ featured: true });
+  const products = await Product.find({ name: 'vase table' });
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products testing route' });
+  const products =  await Product.find(req.query);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
