e6a315e3d28b65e277f82b66608d8fa448581fcd
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-09-17 23:00:10 +0530

    manual muber sort

--- 4-StoreApi/controllers/products.js
+++ 4-StoreApi/controllers/products.js
@@ -1,12 +1,11 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({})
-    .sort('name')
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort('price')
     .select('name price')
-    .limit(4)
-    .skip(1); //skiping he first item
-  res.status(200).json({ products, nbHits: products.length });
+
+    res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
   const { featured, company, name, sort, fields } = req.query;
