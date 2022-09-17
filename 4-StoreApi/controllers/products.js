d2148166d3ff4e82377992874da4b963e7bd2021
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-09-17 18:50:25 +0530

    if query is not present than it's give you all the products which are matched and ignore that are not present in models

--- 4-StoreApi/controllers/products.js
+++ 4-StoreApi/controllers/products.js
@@ -6,6 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 const getAllProducts = async (req, res) => {
   const products =  await Product.find(req.query);
+  console.log(req.query);
   res.status(200).json({ products, nbHits: products.length });
 };
 
