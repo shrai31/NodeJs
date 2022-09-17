4fb0799e5594120effc2f198a788851937af57bc
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-09-17 18:10:03 +0530

    express-async-error handle the errors no need to reate the middleware

--- 4-StoreApi/app.js
+++ 4-StoreApi/app.js
@@ -1,1 +1,35 @@
-console.log('04 Store API')
+require('dotenv').config();
+require('express-async-errors');
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
+// middleware
+
+app.use(express.json());
+
+//routes
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">Products Routes</a>');
+});
+
+app.use('/api/v1/products', productsRouter);
+//products route
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 4200;
+const start = async () => {
+  try {
+    //connectDB
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is running on port ${port}....`));
+  } catch (error) {}
+};
+
+start();
---
+++ 4-StoreApi/controllers/products.js
@@ -0,0 +1,12 @@
+const getAllProductsStatic = async (req, res) => {
+    throw new Error('testing async error')
+  res.status(200).json({ msg: 'products testing route' });
+};
+const getAllProducts = async (req, res) => {
+  res.status(200).json({ msg: 'products testing route' });
+};
+
+module.exports = {
+  getAllProductsStatic,
+  getAllProducts,
+};
