f2f10db67717e2fcc212ce27ff37dd7aa2de4896
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-09-17 20:56:15 +0530

    use the sorting to sort name and price

--- 4-StoreApi/controllers/products.js
+++ 4-StoreApi/controllers/products.js
@@ -1,14 +1,11 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const search = 'aaa';
-  const products = await Product.find({
-    name: { $regex: search, $options: 'i' },
-  });
+  const products = await Product.find({}).sort('-name price');
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
@@ -19,8 +16,17 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' };
   }
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject)
+  if(sort){
+    // console.log(sort) // name, -price
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList)
+  }
+  else{
+    result = result.sort('createdAt')
+  }
+  const products = await result
+  // console.log(queryObject);
   res.status(200).json({ products, nbHits: products.length });
 };
 
