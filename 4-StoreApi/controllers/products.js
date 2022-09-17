f41a3b768784c1d71a63a7569d739386c1ab6cca
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-09-17 23:57:05 +0530

    added the numeric value using regex and forEch

--- 4-StoreApi/controllers/products.js
+++ 4-StoreApi/controllers/products.js
@@ -3,12 +3,12 @@ const Product = require('../models/product');
 const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({ price: { $gt: 30 } })
     .sort('price')
-    .select('name price')
+    .select('name price');
 
-    res.status(200).json({ products, nbHits: products.length });
+  res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
@@ -19,6 +19,30 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' };
   }
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$gt',
+      '<': '$lt',
+      '<=': '$lte',
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    // console.log({filters});
+    const options = ['price','rating'];
+    filters = filters.split(',').forEach((item)=>{
+      const [field,operator,value] = item.split('-')
+      if(options.includes(field)){
+        queryObject[field] = {[operator]:Number(value)}
+      }
+    })
+  }
+  console.log(queryObject);
+  
   let result = Product.find(queryObject);
   if (sort) {
     // console.log(sort) // name, -price
