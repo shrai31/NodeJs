6dacf6282f41721d914973c6e8712409298229bf
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-09-17 22:56:40 +0530

    page limit skip euals topagination

--- 4-StoreApi/controllers/products.js
+++ 4-StoreApi/controllers/products.js
@@ -1,7 +1,11 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select('name price');
+  const products = await Product.find({})
+    .sort('name')
+    .select('name price')
+    .limit(4)
+    .skip(1); //skiping he first item
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
@@ -16,20 +20,24 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' };
   }
-  let result = Product.find(queryObject)
-  if(sort){
+  let result = Product.find(queryObject);
+  if (sort) {
     // console.log(sort) // name, -price
     const sortList = sort.split(',').join(' ');
-    result = result.sort(sortList)
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
   }
-  else{
-    result = result.sort('createdAt')
-  }
-  if(fields){
+  if (fields) {
     const fieldsList = fields.split(',').join(' ');
-    result = result.select(fieldsList)
+    result = result.select(fieldsList);
   }
-  const products = await result
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+  const products = await result;
   // console.log(queryObject);
   res.status(200).json({ products, nbHits: products.length });
 };
