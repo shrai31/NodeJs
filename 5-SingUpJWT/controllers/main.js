85830e23902eb53fa30acb399712386008bb8b5c
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-09-18 13:14:21 +0530

    auth is done sign and verify both

--- 5-SingUpJWT/controllers/main.js
+++ 5-SingUpJWT/controllers/main.js
@@ -1,4 +1,4 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 const CustomAPIError = require('../errors/custom-error');
 
 const login = async (req, res) => {
@@ -8,32 +8,35 @@ const login = async (req, res) => {
     throw new CustomAPIError(`username or password not empty`, 400);
   }
 
- //just for demo, normally provided by DB!!!!
- const id = new Date().getDate()
- // try to keep payload small, better experience for user
- // just for demo, in production use long, complex and unguessable string value!!!!!!!!!
- console.log(id,process.env.JWT_SECRET)
- const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
-   expiresIn: '30d',
-  })
-  
-console.log({token})
- res.status(200).json({ msg: 'user created', token })
+  //just for demo, normally provided by DB!!!!
+  const id = new Date().getDate();
+  // try to keep payload small, better experience for user
+  // just for demo, in production use long, complex and unguessable string value!!!!!!!!!
+  console.log(id, process.env.JWT_SECRET);
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+  res.status(200).json({ msg: 'user created', token });
 };
 
 const dashboard = async (req, res) => {
-
   const authHeader = req.headers.authorization;
-  if(!authHeader || !authHeader.startsWith('Bearer ')){
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
     throw new CustomAPIError(`No token provided`, 401);
   }
-  const token = authHeader.split(' ')[1]
+  const token = authHeader.split(' ')[1];
   console.log(token);
-  const luckyNumber = Math.floor(Math.random() * 100);
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
-    msg: `Hello, Him Rai`,
+    msg: `Hello, ${decoded.username}`,
     secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
   });
+  } catch (error) {
+    throw new CustomAPIError(`No authorized to access this route`, 401);
+  }
 };
 
 module.exports = {
