fca2453d3a246948f7782d8e0118cf1365d10c65
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-09-19 22:32:19 +0530

    initial setup JobAPi

--- 5-SingUpJWT/middleware/error-handler.js
+++ 5-SingUpJWT/middleware/error-handler.js
@@ -1,5 +1,6 @@
 const {CustomAPIError} = require('../errors')
 const { StatusCodes } =require('http-status-codes')
+
 const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: err.message })
