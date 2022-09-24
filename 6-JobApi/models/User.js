afd372d132a4f2d401abb28ff6a1674e19a0e702
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-09-24 22:43:48 +0530

    no need of next after moongoose 5

--- 6-JobApi/models/User.js
+++ 6-JobApi/models/User.js
@@ -24,10 +24,9 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre('save',async function (next){
+userSchema.pre('save',async function (){
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt)
-  next();
 })
 
 module.exports = mongoose.model('User', userSchema);
