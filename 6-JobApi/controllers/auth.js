cd52941d600fc9da6d50e9ce4d2ff91331448d72
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-09-24 22:42:36 +0530

    bcrypt using middleware in model

--- 6-JobApi/controllers/auth.js
+++ 6-JobApi/controllers/auth.js
@@ -4,15 +4,16 @@ const { BadRequestError } = require('../errors');
 const bcrypt = require('bcryptjs');
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
+  // const { name, email, password } = req.body;
   //this is optional we are using now moongose validator
   //   if (!name || !email || !password) {
   //     throw new BadRequestError('One of the missing name , email, password');
   //   }
-  const salt = await bcrypt.genSalt(10);
-  const hashPassword = await bcrypt.hash(password, salt);
-  const tempUser = { name, email, password: hashPassword };
-  const user = await User.create({ ...tempUser });
+  // create the middleware for hash in model User.js
+  // const salt = await bcrypt.genSalt(10);
+  // const hashPassword = await bcrypt.hash(password, salt);
+  // const tempUser = { name, email, password: hashPassword };
+  const user = await User.create({ ... req.body });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
