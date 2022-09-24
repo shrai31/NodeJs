45657cad08d02b61a1100c3eaf54e34bff4428f0
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-09-24 20:08:01 +0530

    basic routes and controller setup

---
+++ 6-JobApi/controllers/jobs.js
@@ -0,0 +1,26 @@
+const getAllJobs = async (req, res) => {
+  res.send('get All Job');
+};
+
+const getJob = async (req, res) => {
+  res.send('get Job');
+};
+
+const createJob = async (req, res) => {
+  res.send('create Job');
+};
+
+const updateJob = async (req, res) => {
+  res.send('update Job');
+};
+const deleteJob = async (req, res) => {
+  res.send('delete Job');
+};
+
+module.exports = {
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+};
