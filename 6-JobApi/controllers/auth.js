6a5c14d52420d25c5103e25c747b87aa63eb9282
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-09-24 23:21:57 +0530

    jwt token initial setup

--- 6-JobApi/controllers/auth.js
+++ 6-JobApi/controllers/auth.js
@@ -1,20 +1,14 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError } = require('../errors');
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
-  // const { name, email, password } = req.body;
-  //this is optional we are using now moongose validator
-  //   if (!name || !email || !password) {
-  //     throw new BadRequestError('One of the missing name , email, password');
-  //   }
-  // create the middleware for hash in model User.js
-  // const salt = await bcrypt.genSalt(10);
-  // const hashPassword = await bcrypt.hash(password, salt);
-  // const tempUser = { name, email, password: hashPassword };
-  const user = await User.create({ ... req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const user = await User.create({ ...req.body });
+  const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtSecret', {
+    expiresIn: '30d',
+  });
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
