bb73b18ae858209e91c7be21820f15f90863cc83
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-09-25 13:33:16 +0530

    get All Job

--- 6-JobApi/controllers/jobs.js
+++ 6-JobApi/controllers/jobs.js
@@ -3,7 +3,8 @@ const { BadRequestError, NotFoundError } = require('../errors');
 const Job = require('../models/Job');
 
 const getAllJobs = async (req, res) => {
-  res.send('get All Job');
+  const jobs = await Job.find({ createdBy: req.user.userID }).sort('createdAt');
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
 const getJob = async (req, res) => {
