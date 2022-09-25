e62dd0f5ae311268f79f23bb23c8ac0061aace8b
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-09-25 10:42:10 +0530

    add authenticate middleware

--- 6-JobApi/app.js
+++ 6-JobApi/app.js
@@ -3,13 +3,15 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
-//connectDB
-const connectDB = require('./db/connect')
+//common auth middleware 
+const authenticateUser = require('./middleware/authentication');
 
+//connectDB
+const connectDB = require('./db/connect');
 
 //routes
-const authRouter = require('./routes/auth')
-const jobsRouter = require('./routes/jobs')
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
@@ -20,7 +22,7 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/auth',authRouter)
-app.use('/api/v1/jobs',jobsRouter)
+app.use('/api/v1/jobs',authenticateUser,jobsRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
