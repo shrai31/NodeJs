dfb816f4b1afc6aef6fa1b342f781601491c5d7c
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-10-08 15:32:44 +0530

    security

--- 6-JobApi/app.js
+++ 6-JobApi/app.js
@@ -3,6 +3,12 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// extra security packages
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+
 //common auth middleware 
 const authenticateUser = require('./middleware/authentication');
 
@@ -18,6 +24,15 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
+
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+}))
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 // extra packages
 
 // routes
