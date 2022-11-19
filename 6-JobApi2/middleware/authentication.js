8d49a8fb59470b388cbeec25ef95ade7fcca7639
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-11-19 12:04:57 +0530

    add the authentication value to test user

--- 6-JobApi2/middleware/authentication.js
+++ 6-JobApi2/middleware/authentication.js
@@ -13,8 +13,10 @@ const auth = async (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
     // attach the user to the job routes
-    req.user = { userId: payload.userId, name: payload.name }
-    next()
+    // add testUser id
+    const testUser = payload.userId === '63785fc96abba8c354e1f493'
+    req.user = { userId: payload.userId, testUser }
+    next();
   } catch (error) {
     throw new UnauthenticatedError('Authentication invalid')
   }
