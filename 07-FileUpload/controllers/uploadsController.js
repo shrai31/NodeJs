585f9caae8bebe1730be4c3f7328a2c6ba328c9e
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-11-22 23:47:01 +0530

    add image to server

--- 07-FileUpload/controllers/uploadsController.js
+++ 07-FileUpload/controllers/uploadsController.js
@@ -1,9 +1,20 @@
 const { StatusCodes } = require('http-status-codes');
 
+const path = require('path');
+
 const UploadProductImage = async (req, res) => {
-  res.send('Upload product image');
+  let productImage = req.files.image;
+  console.log(productImage);
+  const imagePath = path.join(
+    __dirname,
+    '../public/uploads/' + `${productImage.name}`
+  );
+  console.log(imagePath);
+  await productImage.mv(imagePath);
+  return res.status(StatusCodes.OK).json({image:{src:`/uploads/${productImage.name}`}})
+
 };
 
 module.exports = {
- UploadProductImage
+  UploadProductImage,
 };
