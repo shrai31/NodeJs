0ad778c1a0168844c51a2e0d23898456aa12700b
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-11-23 12:22:39 +0530

    add custom error to file upload

--- 07-FileUpload/controllers/uploadsController.js
+++ 07-FileUpload/controllers/uploadsController.js
@@ -1,18 +1,33 @@
 const { StatusCodes } = require('http-status-codes');
-
 const path = require('path');
+const CustomError = require('../errors');
 
 const UploadProductImage = async (req, res) => {
+  //check file exists or not
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No file Uploaded');
+  }
   let productImage = req.files.image;
-  console.log(productImage);
+  // check the format of images
+  if (!productImage.mimetype.startswith('images')) {
+    throw new CustomError.BadRequestError('Please Upload Image');
+  }
+
+  const maxSize = 1000;
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      'Please upload image smaller than 1KB'
+    );
+  }
   const imagePath = path.join(
     __dirname,
     '../public/uploads/' + `${productImage.name}`
   );
   console.log(imagePath);
   await productImage.mv(imagePath);
-  return res.status(StatusCodes.OK).json({image:{src:`/uploads/${productImage.name}`}})
-
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
 };
 
 module.exports = {
--- 1-Basic-NodeJs/08.OS_Module.js
+++ 1-Basic-NodeJs/08.OS_Module.js
@@ -5,7 +5,6 @@ const user = os.userInfo()
 console.log(user)
 
 // method returns the system uptime in seconds
-console.log(`The System Uptime is ${os.uptime()} seconds`)
 
 const currentOS = {
   name: os.type(),
