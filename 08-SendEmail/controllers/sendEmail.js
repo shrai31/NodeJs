8bb1a0f818d4a6889b1b89db78847a79f036dccf
“himanshu.rai” <himanshu.rai@appinventiv.com>
2022-11-23 15:26:31 +0530

    ethereal email and node mailer

--- 08-SendEmail/controllers/sendEmail.js
+++ 08-SendEmail/controllers/sendEmail.js
@@ -1,5 +1,27 @@
-const sendEmail = (req, res)=>{
-    res.send('send email func');
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (req, res) => {
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: 'geoffrey.kessler@ethereal.email',
+      pass: 'K1um6jKMgAfWtbJ1J8',
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Himanshu Rai" <shrai31@example.com>', // sender address
+    to: 'baz@example.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello Baz?', // plain text body
+    html: '<h2>Sending email with nodejs</h2>', // html body
+  });
+  res.json(info);
 };
 
 module.exports = sendEmail;
